58bc45f132c4f240b8fcbc9fd06124441ea65591
seamusic43 <seamusic43@gmail.com>
2023-07-12 17:41:48 +0900

    ESlint 설정 및 data binding 연습

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// https://eslint.org/docs/user-guide/configuring
+module.exports = {
+    root: true,
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        sourceType: 'module'
+    },
+    env: { browser: true },
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/standard'
+        // 'standard',
+        // 'plugin:vue/recommended'
+    ],
+    plugins: [
+        // 'html',
+        // 'standard',
+        'vue'
+    ],
+    // add your custom rules here
+    rules: {
+        // allow async-await
+        'generator-star-spacing': 'off',
+        // allow debugger during development
+        indent: ['error', 4],
+        semi: ['error'],
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    }
+}
---
+++ src/views/DataBinding.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello, {{ title }}! </h1>
+</template>
+
+<script>
+export default {
+    name: 'DataBinding',
+    data () {
+        return {
+            title: 'World'
+        }
+    }
+}
+</script>
